7aa814b4b283d41fe21c1836af850a660f18f55d
yangzheng94 <2367854883@qq.com>
2026-08-15 16:53:31 +0800

    fix(server): 移除 prod.ts 中重复的 MIME 键

--- packages/server/src/prod.ts
+++ packages/server/src/prod.ts
@@ -18,7 +18,6 @@ const MIME: Record<string, string> = {
   ".css": "text/css; charset=utf-8",
   ".json": "application/json; charset=utf-8",
   ".webmanifest": "application/manifest+json; charset=utf-8",
-  ".webmanifest": "application/manifest+json; charset=utf-8",
   ".png": "image/png",
   ".jpg": "image/jpeg",
   ".jpeg": "image/jpeg",
